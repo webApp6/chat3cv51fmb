380594f312ead4f1bbeb66465ce3ee6f430cccdf
pwebApp <pwebApp11@gmail.com>
2021-11-18 13:48:52 -0600

    pruebas

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyABCpbdwer_EosjOKZw4VvRSYMnB5Vb40o",
+  authDomain: "fbpruebas.firebaseapp.com",
+  projectId: "fbpruebas",
+  storageBucket: "fbpruebas.appspot.com",
+  messagingSenderId: "275448689839",
+  appId: "1:275448689839:web:66ca10113cd0b51338d05a",
+  measurementId: "G-1S5B6JWJ55"
+    });
